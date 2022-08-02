5c06bc74e423857981fad02ad28b01aa7b931815
chandra <chandra>
2022-08-02 23:03:49 +0530

    adding version number for new release

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_analyticswidget';
 $plugin->release = '0.1.0';
-$plugin->version = 2022062102;
+$plugin->version = 2022062103;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
